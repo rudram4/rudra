bbacc3fd389e37c128134c529f710b16aeb90ed1
rudram <40146741+rudram4@users.noreply.github.com>
2024-07-30 01:12:06 +0530

    Update chandrahasa.sh

--- chandrahasa.sh
+++ chandrahasa.sh
@@ -1,25 +1,65 @@
 #!/bin/bash
-echo -e "_________ .__                       .___             .__                                    
-\_   ___ \|  |__ _____    ____    __| _/___________  |  |__ _____    ___________            
-/    \  \/|  |  \\__  \  /    \  / __ |\_  __ \__  \ |  |  \\__  \  /  ___/\__  \    ______ 
-\     \___|   Y  \/ __ \|   |  \/ /_/ | |  | \// __ \|   Y  \/ __ \_\___ \  / __ \_ /_____/ 
- \______  /___|  (____  /___|  /\____ | |__|  (____  /___|  (____  /____  >(____  /         
-        \/     \/     \/     \/      \/            \/     \/     \/     \/      \/          
-__________                                                                                  
-\______   \_____ ___  _______  _____    ____                                                
- |       _/\__  \\  \/ /\__  \ \__  \  /    \                                               
- |    |   \ / __ \\   /  / __ \_/ __ \|   |  \                                              
- |____|_  /(____  /\_/  (____  (____  /___|  /                                              
-        \/      \/           \/     \/     \/                                               "
-web=$1
-echo $OF
-if [ $web == "-h" ]
-then
-	echo "Usage: ./chandrahasa website.com"
-else
-	./subdomainer -t $web -f true
-	cd $web
-	cd $(date +"%m-%d-%Y")
-	nuclei -t /root/nuclei-templates/ -l all-live.txt -es info -o nucleiall.txt
-	cat all.txt | gauplus -subs -b png,jpg,gif,jpeg,swf,woff,gif,svg -o allUrls.txt ; cat allUrls.txt | httpx -mc 200,403 -o liveallurls.txt
+
+while getopts ":t:l:d:h" opt; do
+    case $opt in
+        t)
+            IFS=',' read -ra domains <<< "$OPTARG"
+            ;;
+        l)
+            if [ -r "$OPTARG" ]; then
+                mapfile -t domains < "$OPTARG"
+            else
+                echo "Error: Unable to read domain list file '$OPTARG'."
+                exit 1
+            fi
+            ;;
+        d)
+            output_directory="$OPTARG"
+            ;;
+        h)
+            echo "Usage: ./chandrahasa [-t domain1,domain2,... | -l domain_list_file] -d output_directory"
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${#domains[@]}" -eq 0 ]; then
+    echo "Error: No domains provided."
+    exit 1
 fi
+
+if [ -z "$output_directory" ]; then
+    echo "Error: Output directory not specified."
+    exit 1
+fi
+
+mkdir -p "$output_directory"
+
+for domain in "${domains[@]}"; do
+    echo -e "Processing domain: $domain"
+
+    # Subdomain Enumeration
+    ./subdomainer -t "$domain" -f true
+
+    # Change to the domain directory
+    mv "$domain" "$output_directory"
+
+    echo -e "Completed processing domain: $domain"
+    echo "----------------------------------"
+done
+
+cd "$output_directory"
+
+# Collect all all-live.txt files from domain directories and combine them into one file
+find . -mindepth 2 -type f -name "all-live.txt" -exec sh -c 'cat {} >> combined-all-live.txt' \;
+
+# Nuclei Scanning on collected-all-live.txt
+nuclei -l combined-all-live.txt -es info -o nucleiall.txt
+
+cd ..
+
+echo "All results collected in '$output_directory'."
